cecb1243acd9aa13eb8a589d655e5054f842c885
SlawaSlawa <forstudyandwork.slawa@yandex.ru>
2025-11-01 10:47:51 +0400

    added vue-router

--- src/main.js
+++ src/main.js
@@ -2,7 +2,9 @@ import { createApp } from "vue";
 import "@/assets/css/bootstrap-reboot.min.css";
 import "@/assets/css/fonts.css";
 import "@/assets/css/main.css";
-import store from "./store/index.js";
+import router from "./router";
+import store from "./store";
+
 import App from "./App.vue";
 
-createApp(App).use(store).mount("#app");
+createApp(App).use(store).use(router).mount("#app");
